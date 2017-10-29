6cbeed645585f2e9dc3a44cede0ee3803a3694f3
William Hallatt <goblincoding@gmail.com>
2017-10-29 11:14:45 +1000

    fix/ remove disallowed directive from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
-import {log} from "util";
-import {HeaderComponent} from "./header/header.component";
+import { log } from 'util';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  directives: [HeaderComponent]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  title = 'Loaded D3 v'+d3.version;
+export class AppComponent implements OnInit {
+  title = 'Loaded D3 v' + d3.version;
 
   ngOnInit() {
 
